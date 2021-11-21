c631ce66a93b52642fbddb4520a3dffc99b3dce6
Dan Saul <dan@dsaul.ca>
2021-11-21 13:15:33 -0600

    DatabaseBootstrap Skeleton

---
+++ C#/DatabaseBootstrap/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseBootstrap
+{
+	public class Program
+	{
+		public static void Main(string[] args) {
+			Console.WriteLine("Dispatch Pulse Database Bootstrap");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			// We don't want docker to keep relaunching this program,
+			// so if it got to this point, sleep repeatedly forever.
+			while (true) {
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
