59ce9a190e2453dbf852a07cc074f42bc7886b0e
Dan Saul <dan@dsaul.ca>
2022-03-26 17:13:28 -0500

    minio

---
+++ Docker/build/dispatch-pulse-ce-minio-bootstrap/minio-bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+#set -o xtrace
+set -o pipefail
+
+# Change Directory to script directory.
+cd $(dirname "${BASH_SOURCE[0]}")
+
+sleep 3s
+
+mc alias set minio $MINIO_HOST $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc mb minio/pdflatex-compiled-pdfs
+mc mb minio/card-on-file-authorization-forms
+mc mb minio/tts-cache
