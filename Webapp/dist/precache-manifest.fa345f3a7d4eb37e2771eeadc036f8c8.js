33513f8584ce5b6fc17d0236279fef94129a0d68
Dan Saul <dan@dsaul.ca>
2022-04-09 22:36:02 -0500

    dist

---
+++ Webapp/dist/precache-manifest.fa345f3a7d4eb37e2771eeadc036f8c8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96d8c0a7f31737fe0e3",
+    "url": "/css/app.3be1f0db.css"
+  },
+  {
+    "revision": "1d34375c897e22085217",
+    "url": "/css/chunk-vendors.14c2db47.css"
+  },
+  {
+    "revision": "71781c5621406e8a3b9526e8dcb65b77",
+    "url": "/favicon.svg"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/fonts/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/fonts/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/fonts/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/fonts/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "0ea33f022d8d0135935a74e74e78af8a",
+    "url": "/fonts/materialdesignicons-webfont.0ea33f02.woff"
+  },
+  {
+    "revision": "35deb31494f16d8fec73a8ee77a32a2d",
+    "url": "/fonts/materialdesignicons-webfont.35deb314.eot"
+  },
+  {
+    "revision": "aac01880baac84cde28d2a48fbbcd8fa",
+    "url": "/fonts/materialdesignicons-webfont.aac01880.ttf"
+  },
+  {
+    "revision": "e2143a48f593e0ae22efba8a87501064",
+    "url": "/fonts/materialdesignicons-webfont.e2143a48.woff2"
+  },
+  {
+    "revision": "71781c5621406e8a3b9526e8dcb65b77",
+    "url": "/img/Dispatch Pulse Logo v1.71781c56.svg"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/img/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/img/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "893d117209036c90d2d293bd0e7f53db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96d8c0a7f31737fe0e3",
+    "url": "/js/app.fe910516.js"
+  },
+  {
+    "revision": "1d34375c897e22085217",
+    "url": "/js/chunk-vendors.067e6f48.js"
+  },
+  {
+    "revision": "d4d81889d6296ae2d0e4bde72c82ed30",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
